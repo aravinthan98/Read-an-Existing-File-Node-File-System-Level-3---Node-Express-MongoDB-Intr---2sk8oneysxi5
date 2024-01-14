08a0a8237e6983952cefbff41c3d168f9c316475
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2024-01-14 08:15:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
-  
+
+try {   
+    const content = await fs.readFile(filename, "utf-8");
+    return content;
+  } catch (error) {
+    console.error("Error", error.message);
+    throw error;
+  }
 };
 
 
